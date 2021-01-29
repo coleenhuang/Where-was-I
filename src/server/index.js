c12df3e8181be8b91bc7d08d85a9513a36671575
Coleen Huang <coleenhuang@gmail.com>
2021-01-29 14:39:51 -0800

    initial setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const os = require('os');
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
