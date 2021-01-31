553112626786953a5b9678380923ea3ad3a366a6
Coleen Huang <coleenhuang@gmail.com>
2021-01-31 12:36:52 -0800

    set up pg pool

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('Respond with a resource');
+    res.send('users');
 });
 
 module.exports = router;
