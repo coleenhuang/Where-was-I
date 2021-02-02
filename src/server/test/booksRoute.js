aaeb8553cce9d94bfc9863cfd0dd224972922648
Coleen Huang <coleenhuang@gmail.com>
2021-02-02 13:31:16 -0800

    set up test for empty book route

--- src/server/test/booksRoute.js
+++ src/server/test/booksRoute.js
@@ -1,11 +1,24 @@
 const { expect } = require('chai');
+const { pool } = require('../config')
 const supertest = require('supertest');
 const app = require('../app');
 
-describe('Express App', () => {
-  it('should return a message from GET /books', () => {
-    return supertest(app)
-      .get('/books')
-      .expect(200, 'here');
-  });
+describe('Book routes', () => {
+  before('clean the table', () => 
+    pool.query('TRUNCATE users, books, chapters, read_chapters RESTART IDENTITY CASCADE')
+  )
+  afterEach('cleanup', () => 
+    pool.query('TRUNCATE users, books, chapters, read_chapters RESTART IDENTITY CASCADE')
+  )
+  
+  describe('GET /books', () => {
+    context('Given no books', () => {
+      it('should return 200 and an empty array', () => {
+      return supertest(app)
+        .get('/books')
+        .expect(200, []);
+      });
+    })
+  })
+  
 });
