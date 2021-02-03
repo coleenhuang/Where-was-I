0e73bf009fc29f595bfcab29ad097537d8e7db68
Coleen Huang <coleenhuang@gmail.com>
2021-02-02 16:49:44 -0800

    added test for /books

---
+++ src/server/test/books.fixtures.js
@@ -0,0 +1,16 @@
+function makeBooksArray() {
+    return [
+        {
+          id: 1,
+          book_name: 'Genesis'
+        },
+        {
+          id: 2,
+          book_name: 'Exodus'
+        }
+      ]
+}
+
+module.exports = {
+    makeBooksArray
+}
--- src/server/test/booksRoute.js
+++ src/server/test/booksRoute.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const { pool } = require('../config')
 const supertest = require('supertest');
+const { makeBooksArray } = require('./books.fixtures')
 const app = require('../app');
 
 describe('Book routes', () => {
@@ -18,6 +19,20 @@ describe('Book routes', () => {
         .get('/books')
         .expect(200, []);
       });
+    })
+    context('Given there are books in the db', () => {
+      const testBooks = makeBooksArray();
+      const query = {
+        text: 'INSERT INTO books(book_name) VALUES($1), ($2)',
+        values: [testBooks[0].book_name, testBooks[1].book_name],
+      }
+      beforeEach(() => pool.query(query))
+
+      it('responds with 200 and all the books', () => {
+        return supertest(app)
+        .get('/books')
+        .expect(200, testBooks);
+      })
     })
   })
   
