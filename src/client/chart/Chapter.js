a9bd4b905087333eaa7aa7a67a53efa10134877b
Coleen Huang <coleenhuang@gmail.com>
2021-02-19 10:57:58 -0800

    added click event handlers for reading progress

--- src/client/chart/Chapter.js
+++ src/client/chart/Chapter.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 //implement onClick handler to indicate read status
 //Half circle for in progress, circle for read
 
 const Chapter = (props) => {
+    const [read, setRead] = useState('unread')
+    const handleClick = () => {
+        if (read === 'unread') {
+            setRead('in-progress')
+        }
+        else if (read === 'in-progress') {
+            setRead('read')
+        }
+        else {
+            setRead('unread')
+        }
+
+    }
     return (
-        <div className='chapter'>
-            {props.num}
+        <div className={`chapter-shape ${read}`} onClick={handleClick}>
+            <p className='chapter'>
+                {props.num}
+            </p>
         </div>
     )
 }
