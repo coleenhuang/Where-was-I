b651a2976a8b4efb568c59dc432d79acb38c971f
Coleen Huang <coleenhuang@gmail.com>
2021-02-23 17:06:22 -0800

    set up front end auth and env variables

--- src/client/App.js
+++ src/client/App.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import './app.css';
 import Chart from './chart/Chart';
+import LoginButton from './loginButton';
+import LogoutButton from './logoutButton';
 
 const App = () => {
   return (
     <div>
+      <LoginButton />
+      <LogoutButton />
       <h1>Where was I?</h1>
       <Chart />
     </div>
--- src/client/index.js
+++ src/client/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Root>
     <BrowserRouter>
-      <App />
+    <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    redirectUri={window.location.origin}
+    audience={process.env.REACT_APP_AUDIENCE}
+    scope="read:current_user update:current_user_metadata"
+    >
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </ Root>, document.querySelector('#root')
 )
