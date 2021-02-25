06285a63ebfe0bb32231c883347271fa58cb4e82
Coleen Huang <coleenhuang@gmail.com>
2021-02-25 10:11:31 -0800

    implemented route to get all users

---
+++ seeds/seed.users.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(username, email, userid) VALUES
+('Frodo Baggins', 'theringbearer@thefellowship.org', 'frodo1234'),
+('Pippin Took', 'pippintook@thefellowship.org', 'pippin1234'),
+('Merry Brandybuck', 'merryb@thefellowship.org', 'merry1234'),
+('Boromir', 'thecaptain@thefellowship.org', 'boromir1234'),
+('Gimli', 'gimli@thefellowship.org', 'gimli1234'),
+('Legolas Greenleaf', 'princeofmirkwood@thefellowship.org', 'legolas1234'),
+('Aragorn Telcontar', 'strider@thefellowship.org', 'aragorn1234'),
+('Gandalf', 'thegreypilgrim@thefellowship.org', 'gandalf1234');
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userControllers = require('../controllers/userControllers')
 
-router.get('/', function(req, res, next) {
-    res.send('users');
-});
+router.get('/', userControllers.user_list);
 
 module.exports = router;
