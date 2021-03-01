0c78b074921cb24a795b170ae8a6b98ba811b623
Coleen Huang <coleenhuang@gmail.com>
2021-03-01 11:37:19 -0800

    added protected page component

--- src/client/App.js
+++ src/client/App.js
@@ -4,16 +4,29 @@ import './app.css';
 import Chart from './chart/Chart';
 import LoginButton from './loginButton';
 import LogoutButton from './logoutButton';
-import Profile from './profile';
+import Public from './Public';
+import Protected from './Protected';
+import Login from './Login';
+import PrivateRoute from './privateRoute';
 
+
+import { Route, Link } from 'react-router-dom';
 const App = () => {
   return (
     <div>
       <LoginButton />
       <LogoutButton />
       <h1>Where was I?</h1>
-      <Chart />
-      <Profile />
+      <div>
+        <ul>
+          <li><Link to="/public">Public Page</Link></li>
+          <li><Link to="/protected">Protected Page</Link></li>
+        </ul>
+
+        <Route path="/public" component={Public}/>
+        <Route path="/login" component={Login}/>
+        <PrivateRoute path="/protected" component={Protected} />
+      </div>
     </div>
   )
 }
---
+++ src/client/privateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const {isAuthenticated} = useAuth0();
+    return <Route {...rest} render={(props) => (
+        isAuthenticated === true
+        ? <Component {...props} />
+        : <Redirect to='/login' />
+    )} />}
+
+export default PrivateRoute;
