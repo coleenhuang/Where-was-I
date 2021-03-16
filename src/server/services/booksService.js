867ee11e0e1b1e0ee958d4be412aa55af303272c
Coleen Huang <coleenhuang@gmail.com>
2021-03-16 14:40:10 -0700

    fixed the expect statements for the booksService tests

--- src/server/services/booksService.js
+++ src/server/services/booksService.js
@@ -1,6 +1,12 @@
 const BooksService = {
     getAllBooks(db) {
         return db('books').select('*')
+    },
+    getByTestament(db, testament) {
+        return db('books').where('testament', testament)
+    },
+    getById(db, id) {
+        return db('books').where('id', id).first()
     }
 }
 
--- src/server/test/books-service.spec.js
+++ src/server/test/books-service.spec.js
@@ -1,6 +1,8 @@
+const { expect } = require('chai');
 const knex = require('knex');
 const BooksService = require('../services/booksService');
 const { makeBooksArray } = require('./books.fixtures');
+
 describe('Books service object', () => {
     let db;
     // Prepare the database connection using the `db` variable available
@@ -27,16 +29,66 @@ describe('Books service object', () => {
         it('returns an empty array', () => {
             return BooksService
               .getAllBooks(db)
-              .then(books => expect(books).to.eql([]));
+              .then(books => {
+                expect(books).to.be.a('array');
+                expect(books).to.have.lengthOf(0);
+            });
           });
         context('there is data', () => {
-            before('insert data into books', () => {
-
-            })
+            beforeEach('insert data into books', () => 
+                db('books').insert(testBooks)
+            )
             it('returns all books', () => {
                 return BooksService
                 .getAllBooks(db)
-                .then(books => testBooks)
+                .then(books => {
+                    expect(books).to.be.a('array');
+                    expect(books).to.eql(testBooks)
+                    expect(books).to.have.lengthOf(4);
+                })
+            })
+        })
+    })
+    describe('getTestamentBooks()', () => {
+        it('returns an empty array for OT', () => {
+            return BooksService
+            .getByTestament(db, 'Old')
+            .then(books => {
+                expect(books).to.be.a('array');
+                expect(books).to.have.lengthOf(0);
+            })
+        })
+        it('returns an empty array for NT', () => {
+            return BooksService
+            .getByTestament(db, 'New')
+            .then(books => {
+                expect(books).to.be.a('array');
+                expect(books).to.have.lengthOf(0);
+            })
+        })
+        context('there is data', () => {
+            beforeEach('insert data into books', () => 
+                db('books').insert(testBooks)
+            )
+            it('returns all OT books', () => {
+                const expectedBooks = testBooks.filter(book => book.testament === 'Old')
+                return BooksService
+                .getByTestament(db, 'Old')
+                .then(books => {
+                    expect(books).to.be.a('array');
+                    expect(books).to.eql(expectedBooks);
+                    expect(books).to.have.lengthOf(2);
+                })
+            })
+            it('returns all NT books', () => {
+                const expectedBooks = testBooks.filter(book => book.testament === 'New')
+                return BooksService
+                .getByTestament(db, 'New')
+                .then(books => {
+                    expect(books).to.be.a('array');
+                    expect(books).to.eql(expectedBooks);
+                    expect(books).to.have.lengthOf(2);
+                })
             })
         })
     })
